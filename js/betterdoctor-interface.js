518d45d9374e140c8da453029a929af8d40d8f24
Ben Budinger <budingerbc@gmail.com>
2017-09-15 14:55:06 -0700

    rename file to pratice(s) due to confusion with the way the api worked.

--- js/betterdoctor-interface.js
+++ js/betterdoctor-interface.js
@@ -1,21 +1,47 @@
-import {Doctors} from './../js/doctors.js';
+import {Practices} from './../js/practices.js';
 
 var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-  $('#doctor-search-form').submit(function(e) {
+  $('#practice-search-form').submit(function(e) {
     e.preventDefault();
 
     let illness = $('#illness').val();
-    let name = $('#doctor-name').val();
+    let name = $('#practice-name').val();
 
     if(illness.length === 0 && name.length === 0) {
       alert("Please search by at least one parameter.");
     } else {
-      let doctorList = new Doctors(illness, name);
-      doctorList.getData();
+      let practiceList = new Practices(illness, name);
+      practiceList.getData();
+
+      setTimeout(function() {
+        console.log(practiceList.practices);
+        for (let i = 0; i < practiceList.practices.length; i++) {
+
+          $('#practice-list').append(
+            `<div class="panel panel-default">
+              <div class="panel-heading"><h4>${practiceList.practices[i].profile.first_name + ' ' + practiceList.practices[i].profile.last_name + ', <strong>' + practiceList.practices[i].profile.title + '</strong>'}</h4></div>
+              <div class="panel-body">
+                <div class="row">
+                  <div class="col-md-1">
+                    <img src="${practiceList.practices[i].profile.image_url}">
+                  </div>
+                  <div class="col-md-3">
+                    <ul>
+                      <li><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>${practiceList.practices[i].practices[0].phones[0].number}</li>
+                      <li><span class="glyphicon glyphicon-home" aria-hidden="true"></span>${practiceList.practices[i].practices[0].visit_address.street}</li>
+                      <li><span class="glyphicon glyphicon-home" aria-hidden="true"></span>${practiceList.practices[i].practices[0].visit_address.city + ',' + practiceList.practices[i].practices[0].visit_address.state + ' ' + practiceList.practices[i].practices[0].visit_address.zip}</li>
+                      <li><span class="glyphicon glyphicon-user" aria-hidden="true"></span>${practiceList.practices[i].practices[0].phones[0].number}</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>`);
+
+        }
+
+      }, 2000);
     }
-
-
   });
 });
--- js/practices.js
+++ js/practices.js
@@ -1,8 +1,8 @@
 var apiKey = require('./../.env').apiKey;
 
-export class Doctors {
+export class Practices {
   constructor(illness, name) {
-    this.doctors = [];
+    this.practices = [];
     this.illness = illness;
     this.name = name;
   }
@@ -15,12 +15,13 @@ export class Doctors {
       let request = new XMLHttpRequest();
 
       let url = "";
+      // altering the api request based on the input parameters
       if (that.name.length === 0) {
         url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${that.illness}&location=47.608013,-122.335167,100&user_location=47.428718,-122.321394&skip=0&user_key=${apiKey}`;
       } else if (that.illness.length === 0) {
         url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${that.name}&location=47.608013,-122.335167,100&user_location=47.428718,-122.321394&skip=0&user_key=${apiKey}`;
       } else {
-        url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${that.name}&query=${that.illness}&location=47.608013,-122.335167,100&user_location=47.428718,-122.321394&skip=0&user_key=${apiKey}`;
+        url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${that.name}&query=${that.illness}&location=47.608013,-122.335167,30&user_location=47.428718,-122.321394&skip=0&user_key=${apiKey}`;
       }
 
       request.onload = function() {
@@ -36,11 +37,9 @@ export class Doctors {
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-
       for (let i = 0; i < body.data.length; i++) {
-        that.doctors.push(body.data[i].profile);
+        that.practices.push(body.data[i]);
       }
     });
-    console.log(this.doctors);
   }
 }
